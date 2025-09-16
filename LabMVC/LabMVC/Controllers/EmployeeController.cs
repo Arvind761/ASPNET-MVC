89de3ca6fff4303933e63235d38703d000cb3832
arvind761 <arvindkumarjay07@gmail.com>
2025-09-16 22:31:56 +0530

    Data Pass

---
+++ LabMVC/LabMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string Test()
+        {
+            return "Hello from emp Controller - Test Action method.";
+        }
+       
+    }
+}
